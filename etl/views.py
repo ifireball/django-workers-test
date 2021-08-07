d1b48c0db9f810d0965058aaacb0a08ed66fd4f0
Barak Korren <barak.korren@gmail.com>
2021-08-08 01:31:44 +0300

    Include task information in UI
    With a somewhat unsuccessful attempt as making it all look like a nice
    mobile app without resorting to JS.

--- etl/views.py
+++ etl/views.py
@@ -1,16 +1,32 @@
-from pathlib import Path
 import logging
 
-from django.db import IntegrityError
 from django.shortcuts import render, redirect
+from huey.contrib.djhuey import HUEY
+from huey.exceptions import TaskException
+
 from .models import DataItem
 from .tasks import import_items
 
 logger = logging.getLogger(f"django.app.{__name__}")
 
 
+def all_task_results():
+    for key in HUEY.all_results():
+        try:
+            yield key, False, HUEY.result(key, blocking=False, preserve=True)
+        except TaskException as e:
+            yield key, True, e
+
+
 def list_items(request):
-    return render(request, "etl/list_items.html", {"data_items": DataItem.objects.all()})
+    return render(
+        request, "etl/list_items.html", {
+            "data_items": DataItem.objects.all(),
+            "pending_tasks": HUEY.pending(),
+            "scheduled_tasks": HUEY.scheduled(),
+            "all_task_results": list(all_task_results()),
+        },
+    )
 
 
 def manual_import(request):
